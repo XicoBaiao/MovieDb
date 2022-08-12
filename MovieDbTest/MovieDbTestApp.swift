9968f3391761783f04147a0828faddf75474eaed
XicoBaiao <baiao_maxima@outlook.com>
2022-08-12 15:37:19 +0100

    Initial Commit

---
+++ MovieDbTest/MovieDbTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDbTestApp.swift
+//  MovieDbTest
+//
+//  Created by Baiao, Francisco Fonseca on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieDbTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
